099c125fb674192e1b978c90781ddbd8633c17d3
cwplemons <cw.plemons@gmail.com>
2024-04-23 08:39:34 -0700

    Create YM3812.ino

---
+++ YM3812.ino
@@ -0,0 +1,19 @@
+#define REG_LATCH 2
+#define REG_CLOCK 3
+#define REG_DATA 4
+
+#define SPI_CS 10
+#define SPI_MOSI 11
+#define SPI_MISO 12
+#define SPI_CLK 13
+
+#define YM_IRQ 14
+#define YM_IC 15
+#define YM_A0 16
+#define YM_WR 17
+#define YM_RD 20
+#define YM_CS 21
+
+void setup() {
+  int outputs[] = {REG_CLK};
+}
