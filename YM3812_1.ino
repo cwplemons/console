4dedb2863d5313992c7d37a3cfdd34ab98cb755e
cwplemons <cw.plemons@gmail.com>
2024-05-23 10:01:48 -0700

    Create YM3812_1.ino

---
+++ YM3812_1.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+
+#define IRQ 19
+#define IC 18
+#define ADDR 17
+#define WR 16
+#define RD 15
+#define CS 14
+#define DATA0 1
+#define DATA1 2
+#define DATA2 3
+#define DATA3 4
+#define DATA4 5
+#define DATA5 6
+#define DATA6 7
+#define DATA7 8
+
+void setup() {
+  pinMode(IRQ, OUTPUT);
+  pinMode(IC, OUTPUT);
+  pinMode(ADDR, OUTPUT);
+  pinMode(WR, OUTPUT);
+  pinMode(RD, OUTPUT);
+  pinMode(CS, OUTPUT);
+  pinMode(DATA0, OUTPUT);
+  pinMode(DATA1, OUTPUT);
+  pinMode(DATA2, OUTPUT);
+  pinMode(DATA3, OUTPUT);
+  pinMode(DATA4, OUTPUT);
+  pinMode(DATA5, OUTPUT);
+  pinMode(DATA6, OUTPUT);
+  pinMode(DATA7, OUTPUT);
+  SPI.begin();
+}
